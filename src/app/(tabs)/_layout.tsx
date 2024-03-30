6f7ea68c39d2c23ad6d36bb3af75ff06b23b69c7
Naomy <ndorvillearnold@gmail.com>
2024-03-30 14:19:46 -0400

    added static modal

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,16 +1,21 @@
 // import FontAwesome from "@expo/vector-icons/FontAwesome";
 import { Tabs } from "expo-router";
 import { FontAwesome } from "@expo/vector-icons";
+import { Link } from "expo-router";
 
 export default function TabLayout() {
 	return (
 		<Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
 			<Tabs.Screen
-				name="./timeline/index.tsx"
+				name="index"
 				options={{
 					/*Header titles*/
-					title: "Home",
-
+					title: "header",
+					headerRight: ({ color }) => (
+						<Link href={"/modal"} asChild style={{ marginRight: 20 }}>
+							<FontAwesome name="info-circle" size={24} color="black" />
+						</Link>
+					),
 					/*tab bar icon name */
 					tabBarIcon: ({ color }) => (
 						<FontAwesome size={28} name="home" color={color} />
--- src/components/Tweet.tsx
+++ src/components/Tweet.tsx
@@ -13,44 +13,46 @@ import FontAwesome from "react-native-vector-icons/FontAwesome";
 import { Entypo } from "@expo/vector-icons";
 import { Feather } from "@expo/vector-icons";
 import { EvilIcons } from "@expo/vector-icons";
-
+import { Link } from "expo-router";
 const Tweet = ({ item }) => (
-	<View style={styles.itemContainer}>
-		<Image style={styles.profileImage} source={{ uri: item.imageUrl }} />
+	<Link asChild href="(tabs)/index/details">
+		<View style={styles.itemContainer}>
+			<Image style={styles.profileImage} source={{ uri: item.imageUrl }} />
 
-		<View style={styles.textContainer}>
-			<Text style={styles.title}>{item.title}</Text>
-			<Text>{item.post}</Text>
+			<View style={styles.textContainer}>
+				<Text style={styles.title}>{item.title}</Text>
+				<Text>{item.post}</Text>
 
-			{/* Loop to loop through items, for attachement? means does this propert exist*/}
-			{item.attachment?.map((attachment, attachmentIndex) => (
-				<Image
-					style={styles.postImage}
-					key={attachmentIndex}
-					source={{ uri: attachment.uri }}
-				/>
-			))}
+				{/* Loop to loop through items, for attachement? means does this propert exist*/}
+				{item.attachment?.map((attachment, attachmentIndex) => (
+					<Image
+						style={styles.postImage}
+						key={attachmentIndex}
+						source={{ uri: attachment.uri }}
+					/>
+				))}
 
-			<View
-				style={{
-					flexDirection: "row",
-					padding: 10,
-					justifyContent: "space-evenly",
-				}}
-			>
-				<EvilIcons name="comment" size={24} color="black" />
-				<EvilIcons name="retweet" size={24} color="black" />
-				<Feather name="thumbs-up" size={20} color="black" />
-				<EvilIcons name="chart" size={24} color="black" />
+				<View
+					style={{
+						flexDirection: "row",
+						padding: 10,
+						justifyContent: "space-evenly",
+					}}
+				>
+					<EvilIcons name="comment" size={24} color="black" />
+					<EvilIcons name="retweet" size={24} color="black" />
+					<Feather name="thumbs-up" size={20} color="black" />
+					<EvilIcons name="chart" size={24} color="black" />
+				</View>
 			</View>
+			<Icon
+				name="more-horiz"
+				size={24}
+				color="black"
+				style={styles.iconContainer}
+			/>
 		</View>
-		<Icon
-			name="more-horiz"
-			size={24}
-			color="black"
-			style={styles.iconContainer}
-		/>
-	</View>
+	</Link>
 );
 
 const styles = StyleSheet.create({
